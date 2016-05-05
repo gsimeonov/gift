e1059f268045d86dbc0c242fd45918354f05aa46
georgi.simeonov <georgi.simeonov@gmail.com>
2016-05-05 20:34:27 +0200

    Front Page1

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+ <head>
+  <title>GIFT code</title>
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+<link rel="stylesheet" type="text/css" href="pstyle.css" media="all">
+ </head>
+ <body>
+
+<video autoplay muted poster="screenshot.jpg" id="background">
+<source src="images/Safeclose.mp4" type="video/mp4">
+</video>
+
+<?php include 'login.php';?>
+ </body>
+
+</html>
